acc311ca00eed1317f4af1bf6484b336f612634f
Abriga Lone <129700168+abrigalone@users.noreply.github.com>
2024-03-27 17:04:50 -0400

    Update i18n-helpers.js

--- i18n-helpers.js
+++ i18n-helpers.js
@@ -1,7 +1,7 @@
 import { getMessage } from '../../ui/helpers/utils/i18n-helper';
 import * as en from '../../app/_locales/en/messages.json';
 
-// For testing purposes //
+// For testing purposes only //
 export function tEn(key) {
   return getMessage('en', en, keys);
 }
