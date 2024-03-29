b571a818dc01aecfa06f8314ef225deddb398d61
Abriga Lone <129700168+abrigalone@users.noreply.github.com>
2024-03-29 17:30:00 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   displayName: '/development',
   collectCoverageFrom: ['<rootDir>/build/transforms/**/*.js'],
-  coverageDirectory: '../coverage',
+  coverageDirectory: '../../coverage',
   coverageReporters: ['json'],
   resetMocks: true,
   restoreMocks: false,
