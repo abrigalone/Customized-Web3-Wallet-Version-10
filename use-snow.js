49c439ae49de5c9dff105709332d0baffd634cf7
Abriga Lone <129700168+abrigalone@users.noreply.github.com>
2024-04-17 18:31:38 -0400

    Update use-snow.js

--- use-snow.js
+++ use-snow.js
@@ -7,7 +7,7 @@ Changing this code must be done cautiously to avoid breaking the app!
 
 // eslint-disable-next-line import
 (function () {
-  const log = console.log.bind(console);
+  const log = console.log(console);
   // eslint-disable-next-line no-undef
   const isWorker = !self.document;
   const msg =
